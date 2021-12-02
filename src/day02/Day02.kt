984c1c868b352238cdd218afd654e88e2c09e842
Edrick <52392148+edrick-vgw@users.noreply.github.com>
2021-12-02 14:20:30 +0800

    feat(day02): add solution

---
+++ src/day02/Day02.kt
@@ -0,0 +1,38 @@
+fun main() {
+    fun part1(lines: List<String>): Int {
+        var depth = 0
+        var position = 0
+        for (line in lines) {
+            val (direction, num) = line.split(" ").let { it[0] to it[1].toInt() }
+            when (direction) {
+                "up" -> depth -= num
+                "down" -> depth += num
+                "forward" -> position += num
+            }
+        }
+
+        return position * depth
+    }
+
+    fun part2(lines: List<String>): Int {
+        var aim = 0
+        var depth = 0
+        var position = 0
+        for (line in lines) {
+            val (direction, num) = line.split(" ").let { it[0] to it[1].toInt() }
+            when (direction) {
+                "up" -> aim -= num
+                "down" -> aim += num
+                "forward" -> {
+                    position += num
+                    depth += (num * aim)
+                }
+            }
+        }
+        return position * depth
+    }
+
+    val input = readInput("day02/input")
+    println(part1(input))
+    println(part2(input))
+}
